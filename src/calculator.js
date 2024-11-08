a5b8ef0f16b8bf95675906bb1bdeda4cb6b9f186
Nagamma06 <nagamma@wiznet.io>
2024-11-08 18:24:40 +0530

    Suite 2: passed; code for comma seperated numbers

--- src/calculator.js
+++ src/calculator.js
@@ -3,7 +3,14 @@ const add = (numbers)=>{
     
     if(numbers.length == 1) return parseInt(numbers); // for onle one number 
 
-    
+    // for more than one number
+    else {
+        const delimiter = ",";
+        numbers = numbers.split(new RegExp(delimiter)) //creates an array of strings
+        const numArray = numbers.map(Number); //converts a string array to a number array
+        return numArray.reduce((sum, num) => sum + num, 0); // sum of all numbers
+
+    }
 }
 
 export {add};
