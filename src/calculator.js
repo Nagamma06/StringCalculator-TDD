22b070761b4e77ea01468de71bb96fa3d6a2a128
Nagamma06 <nagamma@wiznet.io>
2024-11-08 18:11:23 +0530

    suite 2: test 1 is passed;expected code is written

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,9 @@
 const add = (numbers)=>{
     if(numbers.length == 0 ) return 0; // for empty string
+    
+    if(numbers.length == 1) return parseInt(numbers); // for onle one number 
+
+    
 }
 
 export {add};
